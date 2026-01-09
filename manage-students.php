69e9fb2df8a43b919ffb9e3c0cebeaf6ec6a6584
Bargui20 <73235824+Bargui20@users.noreply.github.com>
2026-01-09 12:30:42 +0100

    Update manage-students.php

--- manage-students.php
+++ manage-students.php
@@ -2,7 +2,7 @@
 <?php
 session_start();
 error_reporting(0);
-include('includes/config.php');
+include 'includes/config.php';
 if(isset($_SESSION['login']))    {   
     header("Location: index.php"); 
     }
@@ -12,11 +12,11 @@ if(isset($_SESSION['login']))    {
 
 <link rel="stylesheet" type="text/css" href="assets/js/DataTables/datatables.min.css"/>
             <!-- ========== TOP NAVBAR ========== -->
-   <?php include('includes/topbar.php');?> 
+   <?php include 'includes/topbar.php';?> 
             <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
             <div class="content-wrapper">
                 <div class="content-container">
-<?php include('includes/leftbar.php');?>  
+<?php include 'includes/leftbar.php';?>  
 
                     <div class="main-page">
                         <div class="container-fluid">
@@ -115,10 +115,10 @@ foreach($results as $result)
                                                             <td><?php echo htmlentities($result->RegDate);?></td>
                                                             <td><?php echo htmlentities($result->note);?></td>
                                                              <td><?php if($result->Status==1){
-echo htmlentities('Active');
+echo htmlentities('Active';
 }
 else{
-   echo htmlentities('Blocked'); 
+   echo htmlentities('Blocked'; 
 }
                                                                 ?></td>
 <td>
@@ -165,8 +165,9 @@ else{
                 <!-- /.content-container -->
             </div>
             <!-- /.content-wrapper -->
-<?php include('includes/footer.php');?>
+<?php include 'includes/footer.php';?>
 
         
 <?php } ?>
 
+s
